0734c5f575941f5892c6dfd853aca108e0f35230
John Norman <john@suckerfreegames.com>
2015-01-09 22:57:27 -0800

    Updated e-mails some more.

--- app/views/emails/group-register-approve.blade.php
+++ app/views/emails/group-register-approve.blade.php
@@ -5,11 +5,12 @@
 <p>Your GoonID is: {{ $goonid }}<br>
 Your temporary password is: {{ $password }}</p>
 <hr>
-<p>Your GoonID is your login ID for all of our services, be it the forums, Jabber, Mumble, wiki, etc.  This will never change, although you do have the ability to change your display name on the forums.</p>
+<p>Your GoonID is your login ID for all of our services, be it the forums, Jabber, Mumble, wiki, etc.  This will never change, although you do have the ability to change your display name on the forums. This and many initial how-to questions are outlined <a href="https://forums.goonrathi.com/index.php?/forum-4/announcement-6-how-do-i-jabber-mumble-wiki-sponsor-members-make-ops-that-are-too-big-make-poor-life-choices-in-general/">in this handy post!</a></p>
 <p>The first thing you should do is <a href="https://forums.goonrathi.com/index.php?app=core&module=usercp&tab=core&area=email">visit your forum profile and change your password</a>.<br>
 Your password for all of our services is the same, so if you ever forget your password, just reset it on the forums.</p>
 <p>Your next step will be to log into the auth portal again and join a game.  We support multiple games on our system and some games may have different membership requirements, so you will need to go through another authentication cycle.  Just click the "Join Game" button and follow the directions.</p>
 <p>Here are some handy links:<br>
+Membership Rules: <a href="http://www.goonrathi.com/rules.html">http://www.goonrathi.com/rules.html</a><br>
 Auth Portal: <a href="http://goonauth.goonrathi.com/">http://goonauth.goonrathi.com/</a><br>
 Forum: <a href="http://forums.goonrathi.com/">http://forums.goonrathi.com/</a><br>
 Jabber: <a href="http://www.goonrathi.com/jabber.html">http://www.goonrathi.com/jabber.html</a>
