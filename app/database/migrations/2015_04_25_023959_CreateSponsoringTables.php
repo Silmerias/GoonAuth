72b2a3a99fbc2b2c6e73acaf73a26db949519fe6
John Norman <john@suckerfreegames.com>
2015-04-25 00:59:35 -0700

    More MySQL fixes.

--- app/database/migrations/2015_04_25_023959_CreateSponsoringTables.php
+++ app/database/migrations/2015_04_25_023959_CreateSponsoringTables.php
@@ -16,24 +16,18 @@ class CreateSponsoringTables extends Migration {
 		Schema::table('Sponsor', function($t) {
 			// Add our new columns.
 			$t->string('SCode', 10)->unique();
-			$t->integer('SSponsoredID')->nullable();
-		});
-		
-		Schema::table('Sponsor', function($t) {
+			$t->integer('SSponsoredID')->unsigned->nullable();
+
 			// Add new constraints.
 			$t->foreign('SSponsoredID', 'FK_Sponsor_User_Sponsored')
 				->references('UID')->on('User')
 				->onDelete('set null');
-		});
 
-		Schema::table('Sponsor', function($t) {
 			// Break FK contraints on old columns.
 			$t->dropForeign('FK_Sponsor_Group');
 			$t->dropForeign('FK_Sponsor_GameOrg');
 			$t->dropForeign('FK_Sponsor_User_Sponsor');
-		});
 
-		Schema::table('Sponsor', function($t) {
 			// Drop our old columns.
 			$t->dropColumn('GRID');
 			$t->dropColumn('GOID');
@@ -52,25 +46,19 @@ class CreateSponsoringTables extends Migration {
 			// Drop our new columns.
 			$t->dropColumn('SCode');
 			$t->dropColumn('SSponsoredID');
-		});
 
-		Schema::table('Sponsor', function($t) {
 			// Add back the old columns.
 			$t->integer('SSponsorID')->unsigned();	// Sponsoring user.
 			$t->integer('GRID')
 				->unsigned()->nullable();	// Group sponsorship.
 			$t->integer('GOID')
 				->unsigned()->nullable();	// Game Org sponsorship.
-		});
 
-		Schema::table('Sponsor', function($t) {
 			// Add back the old FK constraints.
 			$t->foreign('SSponsorID', 'FK_Sponsor_User_Sponsor')
 				->references('UID')->on('User')
 				->onDelete('cascade');
-		});
 
-		Schema::table('Sponsor', function($t) {
 			$t->foreign('GRID', 'FK_Sponsor_Group')
 				->references('GRID')->on('Group')
 				->onDelete('cascade');
